dcbf7a2eb3e92980996cc1c90395a38fd03b606a
soumya-work <soumyaranjansahuwork@gmail.com>
2025-07-02 12:19:08 +0530

    Filter and Search Functionality

--- app.js
+++ app.js
@@ -7,12 +7,19 @@ document.addEventListener("DOMContentLoaded", () => {
         updateStats();
 
     }
+    //This function loads the data form the local storage and displays the data in frontend  and calls the function like updateTaskList and updateStats...
 })
-let tasks = [];
+
+
+let tasks = []; //This creats an empty array for the tasks
+
+let currentFilter = 'all'; //This sets the current filter to all
+
+//This is an array for the tasks to be stored in todo list
 
 const saveTasks = () => {
     localStorage.setItem('tasks', JSON.stringify(tasks));
-}
+} //saves the array in local storage so call it each time when you do any changes
 
 const addTask = () => {
     const taskInput = document.getElementById("taskInput");
@@ -25,12 +32,18 @@ const addTask = () => {
             completed: false,
         });
         taskInput.value = "";
-        updateTasksList();
-        updateStats();
+        updateTasksList(); // Calls the Update TasksList Function
+        updateStats(); // 
         saveTasks();
 
-    }
+        //If the inpout is text than only push it into the array
+
+    } else {
+        alert("Error : Please enter a valid Text");
+    } // Else display a message 
 };
+
+// The above is a arrow function which is taking the input from the user and pushing the value into the array.
 
 const toggleTaskComplete = (index) => {
     tasks[index].completed = !tasks[index].completed;
@@ -38,14 +51,14 @@ const toggleTaskComplete = (index) => {
     updateStats();
     saveTasks();
 
-};
+}; //Change the task of that index to completed when any achange is made if it is !completed , viceversa
 
 const deleteTask = (index) => {
     tasks.splice(index, 1);
     updateTasksList();
     updateStats();
     saveTasks();
-
+    //This function is called when we click on the delete icon which passes that particular element's index value and then we can use splice method which deletes the number of specified elements from that given index value.
 };
 
 const editTask = (index) => {
@@ -56,34 +69,45 @@ const editTask = (index) => {
     updateTasksList();
     updateStats();
     saveTasks();
-
+    //This function is called when we click on the edit icon which passes that perticular element's index value in the arry and than we can edit it as it put that value into the taskInput bar
 };
 
-const updateStats = () => {
-    const completeTasks = tasks.filter(task => task.completed).length
-    const totalTasks = tasks.length
-    const progress = (completeTasks / totalTasks) * 100
-    const progressBar = document.getElementById("progress")
-    progressBar.style.width = `${progress}% `
 
-    document.getElementById('numbers').innerText = `${completeTasks} / ${totalTasks}`
+
+const updateStats = () => {
+    const completeTasks = tasks.filter(task => task.completed).length //total length of the sub array with the completed tasks.
+    const totalTasks = tasks.length //total length of the main array
+    const progress = (completeTasks / totalTasks) * 100; // converts the progress into percentage
+    const progressBar = document.getElementById("progress") //get the progressbar from html
+    progressBar.style.width = `${progress}% ` //put the width of the progressbar in the html
+
+    document.getElementById('numbers').innerText = `${completeTasks} / ${totalTasks}` //get the numbers elements from the html and put the values i.e length of the arrays.
 
     if (tasks.length && completeTasks === totalTasks) {
         blastConfetti();
     }
-
+    //If the two array for the total task and completed task are eaual then call he blastConfetti function so that we can see the Celebration
 }
 
-const updateTasksList = () => {
+const updateTasksList = (searchText = "") => {
     const taskList = document.getElementById('task-list')
     taskList.innerHTML = '';
 
-    tasks.forEach((task, index) => {
-        const listItem = document.createElement('li');
+    tasks
+        .filter(task => {
+            if (currentFilter === "completed") return task.completed;
+            if (currentFilter === "active") return !task.completed;
+            return true;
+        })
+        .filter(task => {
+            return task.text.toLowerCase().includes(searchText.toLowerCase());
+        })
 
-        listItem.innerHTML = `
+        .forEach((task, index) => {
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `
             <div class="taskItem">
-                <div class = "task ${task.completed ? "completed" : ""}">
+                <div class = "task ${task.completed ? "completed" : ""}"> 
                     <input type ="checkbox" class = "checkbox" ${task.completed ? "checked" : ""} />
                         <p> ${task.text}</p>
                 </div>
@@ -93,16 +117,28 @@ const updateTasksList = () => {
                 </div>
             </div>
         `;
-        listItem.addEventListener('change', () => toggleTaskComplete(index));
-        taskList.appendChild(listItem);
-    });
+            listItem.addEventListener('change', () => toggleTaskComplete(index)); //here the toggleTaskComplete function is called whenever any change is made to the listItem
+            taskList.appendChild(listItem);
+        });
 };
 
 document.getElementById('newTask').addEventListener('click', function (e) {
-    e.preventDefault();
-
-    addTask();
+    e.preventDefault(); //prevents the default behaviour of the form
+    addTask(); // calls the add task function
 });
+
+document.getElementById('filterSelect').addEventListener('change', (e) => {
+    currentFilter = e.target.value;
+    updateTasksList();
+});
+
+
+
+document.getElementById("searchInput").addEventListener("input", (e) => {
+    const query = e.target.value.toLowerCase(); // user ka search text
+    updateTasksList(query); // search query ke saath update
+});
+
 
 const blastConfetti = () => {
     const count = 200,
